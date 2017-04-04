8a37d9e65e9296eccf52a313aeb4e9f6ee926bfd
Kevin Pleitez <pleitez95geova@gmail.com>
2017-04-04 11:57:43 -0600

    se agrego el proyecto de clases DataLayer y la clase conexion para la conexion a la base de datos

---
+++ Gestor de Hospital/DataLayer/Conexion.cs
@@ -0,0 +1,57 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public sealed class Conexion
+    {
+        private MySqlConnection connection = new MySqlConnection();
+        private static Conexion instance = null;
+        private static readonly object padlock = new object();
+
+        Conexion()
+        {
+            connection.ConnectionString = "Server=localhost;Database=hospital;Uid=root;Pwd=admin;";
+            
+        }
+
+        public void Connect()
+        {
+            if(connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void Disconnect()
+        {
+ 
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+
+        }
+
+        public static Conexion getInstance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Conexion();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+
+    }
+}
