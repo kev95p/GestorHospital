57efd7e2488a69b1ec99f01d1cc7d832d2325c41
Kevin Pleitez <pleitez95geova@gmail.com>
2017-04-04 13:30:30 -0600

    se reupero datalayer

---
+++ Gestor de Hospital/DataLayer/Conexion.cs
@@ -0,0 +1,69 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public sealed class Conexion
+    {
+        private MySqlConnection connection = new MySqlConnection();
+        private MySqlDataAdapter adapter;
+
+        private static Conexion instance = null;
+        private static readonly object padlock = new object();
+
+        Conexion()
+        {
+            connection.ConnectionString = "Server=localhost;Database=hospital;Uid=root;Pwd=admin;";
+            
+        }
+
+        private void Connect()
+        {
+            if(connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        private void Disconnect()
+        {
+ 
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+
+        }
+
+        public static Conexion getInstance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Conexion();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public DataSet Consulta(string sentencia)
+        {
+            this.Connect();
+            adapter = new MySqlDataAdapter(new MySqlCommand(sentencia));
+            MySqlCommandBuilder cmb = new MySqlCommandBuilder(adapter);
+            DataSet dt = new DataSet();
+
+            adapter.Fill(dt);
+            return dt;
+        }
+    }
+}
