32d1ffcfd6c81609ed62d3eb49ade7ff1c5dc80a
Kevin Pleitez <pleitez95geova@gmail.com>
2017-04-04 13:44:43 -0600

    Se agrego el metodo OBTENER_USUARIO a la clase Operaciones

--- Gestor de Hospital/DataLayer/Operaciones.cs
+++ Gestor de Hospital/DataLayer/Operaciones.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataLayer
 {
-    class Operaciones
+    public static class Operaciones
     {
+        public static DataSet OBTENER_USUARIO(string usuario,string clave)
+        {
+            try
+            {
+                Conexion con = Conexion.getInstance;
+                DataSet datos = new DataSet();
+                string sentencia = @"SELECT usuario
+                FROM usuarios WHERE clave=md5('" + clave + "') AND usuario = '" + usuario + "'";
+                datos = con.Consulta(sentencia);
+                return datos;
 
+            }
+            catch
+            {
+
+                return new DataSet();
+            }
+        }
     }
 }
