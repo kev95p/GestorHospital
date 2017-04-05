0d206a7fe974f2d982788055058cceb9bf98721d
Kevin Pleitez <pleitez95geova@gmail.com>
2017-04-05 13:48:58 -0600

    se agrego el string ipserver en resources del proyecto datalayer

--- Gestor de Hospital/DataLayer/Conexion.cs
+++ Gestor de Hospital/DataLayer/Conexion.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+﻿using DataLayer.Properties;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -12,13 +13,13 @@ namespace DataLayer
     {
         private MySqlConnection connection = new MySqlConnection();
         private MySqlDataAdapter adapter;
-
+        public string server = Resources.ipserver;
         private static Conexion instance = null;
         private static readonly object padlock = new object();
 
         Conexion()
         {
-            connection.ConnectionString = "Server=localhost;Database=hospital;Uid=root;Pwd=admin;";
+            connection.ConnectionString = "Server="+server+";Database=hospital;Uid=root;Pwd=admin;";
             
         }
 
@@ -76,11 +77,12 @@ namespace DataLayer
 
         public DataTable Consulta(string sentencia)
         {
-            this.Connect();
+            Connect();
             adapter = new MySqlDataAdapter(sentencia , connection);
             MySqlCommandBuilder cmb = new MySqlCommandBuilder(adapter);
             DataTable dt = new DataTable();
             adapter.Fill(dt);
+            Disconnect();
             return dt;
         }
     }
