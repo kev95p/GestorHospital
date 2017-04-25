b2a41b1a3fb78edb4806924bd7db6cf614130c2c
Kevin Pleitez <pleitez95geova@gmail.com>
2017-04-25 14:50:31 -0600

    Se cambio el sessiomanager ahora es un proyecto

---
+++ Gestor de Hospital/SessionManager/SessionManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SessionManager
+{
+    public sealed class SessionManager
+    {
+        private static SessionManager instace= null;
+        private static readonly object padlock = new object();
+
+        private String usuario;
+        
+
+        SessionManager()
+        {
+                    
+        }
+
+        public static SessionManager GetInstance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if(instace == null)
+                    {
+                        instace = new SessionManager();
+                    }
+                    return instace;
+                }
+            }
+        }
+
+        public string Usuario { get => usuario; set => usuario = value; }
+    }
+}
