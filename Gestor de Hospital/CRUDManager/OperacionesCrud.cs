d6bebf75be8eb93a993607efad7485d373a95875
Kevin Pleitez <pleitez95geova@gmail.com>
2017-05-09 11:43:15 -0600

    Se agrego el proyecto CRUDManager para los mantenimientos

---
+++ Gestor de Hospital/CRUDManager/OperacionesCrud.cs
@@ -0,0 +1,34 @@
+﻿using DataLayer.Intefaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDManager
+{
+    public class OperacionesCrud : ICrud<IComando>
+    {
+        public int Insert(IComando obj)
+        {
+            
+            return obj.Execute();
+        }
+
+        public int Update(IComando obj)
+        {
+            return obj.Execute();
+        }
+
+        public int Delete(IComando obj)
+        {
+            return obj.Execute();
+        }
+
+        public DataSet Select(IComando obj)
+        {
+            return obj.Select();
+        }
+    }
+}
