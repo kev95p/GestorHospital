4fc9f53bdb13b29f307ad79feb65a72f83eaef2e
Er1ck6aldamez <ebalmore.gs@gmail.com>
2017-06-06 14:34:14 -0600

    la cague y sobre escribi una onda, pero hoy si ya funiona :V modificacion sobre el Obtner_medicametos

--- Gestor de Hospital/CacheManager/Cache.cs
+++ Gestor de Hospital/CacheManager/Cache.cs
@@ -134,6 +134,24 @@ namespace CacheManager
             }
         }
 
+        public static DataSet OBTENER_PACIENTES()
+        {
+            try
+            {
+                DataSet datos = new DataSet();
+                CommandBuilder cb = new CommandBuilder();
+                string query = @"select * from vistapacientes";
+                cb.CommandText = query;
+                datos = cb.Select();
+                return datos;
+            }
+            catch
+            {
+                return new DataSet();
+            }
+        }
+
+
         public static DataSet OBTENER_MEDICAMENTOS()
         {
             try
