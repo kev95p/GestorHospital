30aef7e7be1ff52ec25bc89662c7a174ec24a65e
Er1ck6aldamez <ebalmore.gs@gmail.com>
2017-06-12 17:27:50 -0600

    creacion de clases citas y recetas medicas

---
+++ Gestor de Hospital/CRUDManager/Entidades/Citas.cs
@@ -0,0 +1,71 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDManager.Entidades
+{
+    public class Citas : OperacionesCrud
+    
+    {
+        private string idCita;
+
+        public string IdCita
+        {
+            get { return idCita; }
+            set { idCita = value; }
+        }
+        private string idPaciente;
+
+        public string IdPaciente
+        {
+            get { return idPaciente; }
+            set { idPaciente = value; }
+        }
+        private string idMedico;
+
+        public string IdMedico
+        {
+            get { return idMedico; }
+            set { idMedico = value; }
+        }
+        private string estado;
+
+        public string Estado
+        {
+            get { return estado; }
+            set { estado = value; }
+        }
+        private string fechaCita;
+
+        public string FechaCita
+        {
+            get { return fechaCita; }
+            set { fechaCita = value; }
+        }
+        private string motivos;
+
+        public string Motivos
+        {
+            get { return motivos; }
+            set { motivos = value; }
+        }
+
+        public bool Insertar()
+        {
+            CommandBuilder cb = new CommandBuilder();
+            StringBuilder query = new StringBuilder();
+            query.Append("INSERT INTO hospital.citas (idPaciente, idMedico, estado, fechaCita, motivos) VALUES (");
+            query.Append("'" + IdPaciente + "', '" + IdMedico + "' ,'" + Estado + "','" + FechaCita + "' ,'" + Motivos + "');");
+            cb.CommandText = query.ToString();
+
+            return (Insert(cb) > 0);
+        }
+
+
+
+
+    }
+}
