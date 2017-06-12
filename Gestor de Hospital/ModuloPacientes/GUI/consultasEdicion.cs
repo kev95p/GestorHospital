31933759c4cc3a336f15f25ce4fe7e1dda9da198
Er1ck6aldamez <ebalmore.gs@gmail.com>
2017-06-12 16:33:21 -0600

    btnGuardr codificado

--- Gestor de Hospital/ModuloPacientes/GUI/consultasEdicion.cs
+++ Gestor de Hospital/ModuloPacientes/GUI/consultasEdicion.cs
@@ -17,6 +17,9 @@ namespace ModuloPacientes.GUI
             InitializeComponent();
         }
 
+
+        public string IDPaciente;
+
         private void btnCancelar_Click(object sender, EventArgs e)
         {
             Close();
@@ -26,6 +29,23 @@ namespace ModuloPacientes.GUI
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
+            CRUDManager.Entidades.Consultas consultas = new CRUDManager.Entidades.Consultas();
+            consultas.IdMedico = cbxDoctor.SelectedValue.ToString();
+            consultas.IdPaciente = IDPaciente;
+            consultas.FechaConsulta1 = SessionManager.FechaHelper.FormatearFecha(dtpFechaConsulta.Text);
+
+
+
+            if (consultas.Insertar())
+                {
+                    MessageBox.Show("enfermedad agregada correctamente", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Error inesperado", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
 
         }
 
