a669269a8b4e44a612e00b03b8961c29ec225efa
Er1ck6aldamez <ebalmore.gs@gmail.com>
2017-06-12 16:30:53 -0600

    creacion de la clase consultas y ingreso codificacion del btn guardar consulta

---
+++ Gestor de Hospital/CRUDManager/Entidades/Consultas.cs
@@ -0,0 +1,58 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDManager.Entidades
+{
+    public class Consultas : OperacionesCrud
+    {
+        private string idConsulta;
+
+        public string IdConsulta
+        {
+            get { return idConsulta; }
+            set { idConsulta = value; }
+        }
+        private string idPaciente;
+
+        public string IdPaciente
+        {
+            get { return idPaciente; }
+            set { idPaciente = value; }
+        }
+        private string idMedico;
+
+        public string IdMedico
+        {
+            get { return idMedico; }
+            set { idMedico = value; }
+        }
+        private string FechaConsulta;
+
+        public string FechaConsulta1
+        {
+            get { return FechaConsulta; }
+            set { FechaConsulta = value; }
+        }
+
+
+
+        public bool Insertar()
+        {
+            CommandBuilder cb = new CommandBuilder();
+            StringBuilder query = new StringBuilder();
+            query.Append("INSERT INTO hospital.consultas (idPaciente, idMedico, FechaConsulta) VALUES (");
+            query.Append("'" + IdPaciente + "', '" + IdMedico + "' ,'" + FechaConsulta1 + "');");
+            cb.CommandText = query.ToString();
+
+            return (Insert (cb) > 0);
+        }
+
+
+
+
+    }
+}
